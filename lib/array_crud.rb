0b21874c0775a50e3a92a7daa64ba42113875947
Timothy Rines <tsrines@gmail.com>
2019-09-18 15:11:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ end
 
 def remove_element_from_start_of_array(array)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
-array.pop
+array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
